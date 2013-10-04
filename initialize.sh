f374191e02b63e2bf06952020cae967003deccba
bagnz0r <phill@omega323.com>
2013-10-04 08:19:58 +0200

    Updated initialize.sh to work properly on OSX and any other BSD based system.

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd $(dirname "$0")
 
-find . -type f -not -wholename "*.git*" -exec sed -i "s/SEDNAME/$1/g" {} \;
-find . -type f -not -wholename "*.git*" -exec sed -i "s/SEDPRETTYNAME/$2/g" {} \;
-find . -type f -not -wholename "*.git*" -exec sed -i "s/SEDBRANDING/$3/g" {} \;
+find . -type f -not -wholename "*.git*" -exec sed -i "" "s/SEDNAME/$1/g" {} \;
+find . -type f -not -wholename "*.git*" -exec sed -i "" "s/SEDPRETTYNAME/$2/g" {} \;
+find . -type f -not -wholename "*.git*" -exec sed -i "" "s/SEDBRANDING/$3/g" {} \;
 git add -A
 
 git mv full_SEDNAME.mk "full_$1.mk"
